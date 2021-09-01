6fb10f707bbfe0cb96a67ef4cd6d0a78cd4a19c2
maosan132 <maosan132@gmail.com>
2021-08-31 20:20:03 -0500

    Make the BooksForm component stateful

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../actions';
 
 const categories = [
   'Action',
@@ -10,25 +12,50 @@ const categories = [
   'Sci-Fi',
 ];
 
-const BooksForm = () => (
-  <form>
-    <h4> Title:</h4>
-    <input
-      type="text"
-      name="title"
-      value="{title}"
-    />
-    <h4>Category:</h4>
-    <select
-      name="category"
-      value=""
-    >
-      {categories.map((category) => (
-        <option key={category}>{category}</option>
-      ))}
-    </select>
-    <button type="submit">Submit</button>
-  </form>
-);
+const BooksForm = () => {
+  const [book, setBook] = useState({ title: '', category: '' });
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // setBook({ ...book, [name]: value });
+    if (name === 'title') {
+      title = value;
+      setBook((state) => ({ ...state, title }));
+    }
+    if (name === 'category') {
+      category = value;
+      setBook((state) => ({ ...state, category }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook({ ...book, id: Math.random().toFixed(2) }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4> Title:</h4>
+      <input
+        name="title"
+        value="{title}"
+        type="text"
+        onChange={(e) => handleChange(e)}
+      />
+      <h4>Category:</h4>
+      <select
+        name="category"
+        value="{category}"
+        onChange={(e) => handleChange(e)}
+      >
+        {categories.map((category) => (
+          <option key={category}>{category}</option>
+        ))}
+      </select>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default BooksForm;
