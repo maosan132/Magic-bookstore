6ee688f611973144799702b13e395445aa8dfa79
maosan132 <maosan132@gmail.com>
2021-09-06 14:23:49 -0500

    Implement Bobok comp

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,28 +1,26 @@
 import PropTypes from 'prop-types';
 
-const Book = (props) => {
-  const { bookId, title, category } = props;
-  return (
-    <tr>
-      <td>{bookId}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td>
-        <button
-          type="button"
-          onClick={() => props.remove(book)}
-        >
-          Remove book
-        </button>
-      </td>
-    </tr>
-  );
-};
+const Book = ({ item, removeBook }) => (
+  <tr>
+    <td>{item.id}</td>
+    <td>{item.title}</td>
+    <td>{item.category}</td>
+    <td>
+      <button
+        type="button"
+        onClick={() => removeBook(item)}
+      >
+        Remove book
+      </button>
+    </td>
+  </tr>
+);
 
 Book.propTypes = {
-  bookId: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default Book;
