2db0dd7ade8010c2e0b266f784aa01c7fe994a3b
maosan132 <maosan132@gmail.com>
2021-09-06 22:20:01 -0500

    Implement new filter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const ADD_BOOK = 'ADD_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export function addBook(book) {
   return {
@@ -12,5 +13,12 @@ export function removeBook(book) {
   return {
     type: REMOVE_BOOK,
     book,
+  };
+}
+
+export function changeFilter(filter) {
+  return {
+    type: CHANGE_FILTER,
+    filter,
   };
 }
