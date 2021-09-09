20c7bc5d5091653e587d8ccbd2eb8f1da1cb27ae
maosan132 <maosan132@gmail.com>
2021-09-09 10:02:23 -0500

    Add images

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
 import * as action from '../actions';
 import CategoryFilter from '../components/CategoryFilter';
+import userIcon from '../user-icon.png';
 
 const BookList = () => {
   const books = useSelector((store) => store.bookStoreReducer);
@@ -28,7 +29,7 @@ const BookList = () => {
           <span>BOOKS</span>
           <CategoryFilter filter={handleCategoryFilter} />
         </div>
-        {/* <img src={userIcon} alt="user-icon" /> */}
+        <img src={userIcon} alt="user-icon" />
       </header>
       <main>
         {
